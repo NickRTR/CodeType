2c6e7b05aed7bcd497750bc74b3fcec812162c71
NickRTR <95316598+NickRTR@users.noreply.github.com>
2022-07-25 21:18:56 +0200

    changed divider color to yellow accent

--- src/lib/components/Divider.svelte
+++ src/lib/components/Divider.svelte
@@ -2,7 +2,7 @@
 
 <style>
 	.divider {
-		background: linear-gradient(90deg, hsla(217, 100%, 50%, 1) 0%, hsla(186, 100%, 69%, 1) 100%);
+		background-color: var(--accent);
 		height: 6px;
 	}
 </style>
