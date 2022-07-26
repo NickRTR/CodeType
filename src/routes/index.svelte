f65d08c1d830aba5e3b18a22d0865d4b287bad04
NickRTR <95316598+NickRTR@users.noreply.github.com>
2022-07-26 20:24:49 +0200

    handling mobile error on server

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,46 +1,30 @@
-<script>
-	// components
-	import Input from "$lib/components/Input.svelte";
-	import Stats from "$lib/components/Stats.svelte";
-
-	// modules
-	import { browser } from "$app/env";
-
-	let displayStats = false;
-
-	function onMobile() {
-		if (browser) {
-			return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+<script context="module">
+	export async function load({ session }) {
+		try {
+			if (session.desktop === false) {
+				throw new Error("mobile not supported");
+			}
+		} catch (error) {
+			console.log(error);
+			return {
+				status: 304,
+				redirect: "/mobile"
+			};
 		}
 	}
 </script>
 
+<script>
+	import Input from "$lib/components/Input.svelte";
+	import Stats from "$lib/components/Stats.svelte";
+
+	let displayStats = false;
+</script>
+
 <body>
-	{#if !onMobile()}
-		{#if displayStats}
-			<Stats bind:displayStats />
-		{:else}
-			<Input bind:submitted={displayStats} />
-		{/if}
+	{#if displayStats}
+		<Stats bind:displayStats />
 	{:else}
-		<article class="mobile">
-			<p>Please use this app on Desktop, unless you code on your phone...</p>
-			<img
-				src="https://yt3.ggpht.com/jdxaiUL9R7okC1RlM0XJaMiG5A67ED-UftUbZES8yR53qfjAVT15PucIE675Hc2Zr2N8yVc1Gg=s900-c-k-c0x00ffffff-no-rj"
-				alt="The Rock"
-				title="https://yt3.ggpht.com/jdxaiUL9R7okC1RlM0XJaMiG5A67ED-UftUbZES8yR53qfjAVT15PucIE675Hc2Zr2N8yVc1Gg=s900-c-k-c0x00ffffff-no-rj"
-			/>
-		</article>
+		<Input bind:submitted={displayStats} />
 	{/if}
 </body>
-
-<style>
-	.mobile {
-		margin-inline: 20px;
-	}
-
-	.mobile img {
-		width: 50%;
-		max-width: 200px;
-	}
-</style>
