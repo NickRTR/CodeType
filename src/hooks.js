c8a092f38dfaf6c8ca9f39a33c98396479374b3c
NickRTR <95316598+NickRTR@users.noreply.github.com>
2022-07-29 11:04:31 +0200

    fixed hooks.js

--- src/hooks.js
+++ src/hooks.js
@@ -1,7 +1,7 @@
 export async function handle({ event, resolve }) {
 	const userAgent = event.request.headers.get("user-agent");
 
-	event.locals.desktop = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(userAgent);
+	event.locals.desktop = !/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(userAgent);
 
 	const response = await resolve(event);
 	return response;
