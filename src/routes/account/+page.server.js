a98ad601c159dfb3b417100aeb19b09202ff5c45
NickRTR <95316598+NickRTR@users.noreply.github.com>
2022-09-03 12:47:48 +0200

    fetch stats for dashboard & display average CPM

---
+++ src/routes/account/+page.server.js
@@ -0,0 +1,20 @@
+import { redirect, error } from "@sveltejs/kit";
+import supabase from "$lib/supabase";
+
+export async function load({ parent }) {
+	const { user } = await parent();
+	if (!user) {
+		throw redirect(307, "/account/login");
+	}
+
+	let { data, error: err } = await supabase.from("Stats").select("*");
+
+	if (err) {
+		throw error(400, err.message);
+	}
+
+	return {
+		stats: data,
+		user
+	};
+}
--- src/routes/account/+page.svelte
+++ src/routes/account/+page.svelte
@@ -1,8 +1,20 @@
 <script>
 	export let data;
+
+	function averageCPM() {
+		const CPMs = data.stats.map((stat) => {
+			return stat.CPM;
+		});
+
+		const average = CPMs.reduce((a, b) => a + b, 0) / CPMs.length;
+
+		return average;
+	}
 </script>
 
 <body>
 	<h1>Dashboard</h1>
 	<p>Hello {data.user.email}</p>
+
+	<p>Average CPM: {averageCPM()}</p>
 </body>
