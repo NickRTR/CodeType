0ce63d82bab9feb526deafab8d36fd913dc341af
NickRTR <95316598+NickRTR@users.noreply.github.com>
2022-12-19 21:40:43 +0100

    🍬

--- src/routes/account/verification/+page.js
+++ src/routes/account/verification/+page.js
@@ -3,6 +3,6 @@ import { redirect } from "@sveltejs/kit";
 export async function load({ parent }) {
 	const { user } = await parent();
 	if (user) {
-		throw redirect(308, "/account");
+		throw redirect(307, "/account");
 	}
 }
--- src/routes/account/verification/+page.svelte
+++ src/routes/account/verification/+page.svelte
@@ -1,18 +1,17 @@
 <script>
 	import { page } from "$app/stores";
+	import { onMount } from "svelte";
 
 	const token = new URLSearchParams($page.url.hash.substring(1)).get("access_token");
 
-	if (token) {
-		setCookie();
-	}
-
-	async function setCookie() {
-		fetch("/api/verification", {
-			method: "POST",
-			body: JSON.stringify({ token })
-		});
-	}
+	onMount(async () => {
+		if (token) {
+			await fetch("/api/verification", {
+				method: "POST",
+				body: JSON.stringify({ token })
+			});
+		}
+	});
 </script>
 
 <h1>Waiting for verification...</h1>
